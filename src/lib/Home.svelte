7cce7d7458a8a417dfac51e14be3b7502f687371
krists <37362882+kristsva@users.noreply.github.com>
2023-03-16 15:31:10 +0200

    Update Home.svelte

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,10 +1,10 @@
 <div class="w-full">
 	<div
-		class="text-left md:text-left font-bold text-slate-200 text-4xl md:text-5xl mb-8 md:mb-16 w-full"
+		class="text-center md:text-left font-bold text-slate-200 text-4xl md:text-5xl mb-8 md:mb-16 w-full"
 	>
 		Atrodi sev piemērotāko<br />TV seriālu vai filmu šim<br />vakaram ar AI palīdzību.
 	</div>
-	<div class="flex flex-wrap items-center mr-auto">
+	<div class="text-center md:text-left flex flex-wrap items-center mr-auto">
 		<button
 			on:click
 			class="text-center bg-red-600 hover:bg-red-700 mb-4 flex-none text-white font-bold py-3 px-6 mr-8"
